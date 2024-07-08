f198942c17b912dabe8b1a1e21b6f56a71b05c91
UpamaU <uutpa088@uottawa.ca>
2024-07-07 23:33:31 -0400

    last edits

--- src/About.js
+++ src/About.js
@@ -1,11 +1,18 @@
-// src/About.js
 import React from 'react';
+import './About.css';
 
 const About = () => {
   return (
-    <div>
-      <h2>About Page</h2>
-      <p>Welcome to the about page!</p>
+    <div className="about">
+      <div className="about-card">
+        <h2>Welcome to Meals 4 U!</h2>
+        <p>Meals 4 U is an online community dedicated to food lovers, home cooks, and culinary enthusiasts of all levels.</p>
+      </div>
+      <div className="about-card">
+        <h2>Join Us</h2>
+        <p>Become a part of the community today. Share your favorite recipes...</p>
+        <p>Thank you for being a part of our journey...</p>
+      </div>
     </div>
   );
 };
--- src/Contact.js
+++ src/Contact.js
@@ -1,11 +1,32 @@
-// src/Contact.js
 import React from 'react';
+import './Contact.css';
 
 const Contact = () => {
   return (
-    <div>
-      <h2>Contact Page</h2>
-      <p>Welcome to the contact page!</p>
+    <div className="contact-container">
+      <div className="contact-card">
+        <h2>Welcome to Meals 4 U!</h2>
+        <p>Meals 4 U is an online community dedicated to food lovers, home cooks, and culinary enthusiasts of all levels.</p>
+      </div>
+      <div className="contact-card">
+        <h2>Join Us:</h2>
+        <p>Become a part of the community today. Share your favourite recipes...</p>
+        <p>Thank you for being a part of our journey...</p>
+      </div>
+      <div className="contact-card">
+        <h2>At Meals 4 U, we value our community and are always eager to hear from you.</h2>
+        <p>Whether you have a question, feedback, or just want to share your cooking experiences, feel free to reach out to us.</p>
+      </div>
+      <div className="contact-card">
+        <h2>We appreciate your feedback as it helps us improve and provide the best experience for our users.</h2>
+        <p>Please let us know how we can serve you better.</p>
+        <p>Thank you for reaching out to us. We look forward to hearing from you!</p>
+      </div>
+      <div className="contact-card">
+        <h2>Contact Information</h2>
+        <p>Email: Mealsforu@gmail.com</p>
+        <p>Phone Number: 000-000-0000</p>
+      </div>
     </div>
   );
 };
